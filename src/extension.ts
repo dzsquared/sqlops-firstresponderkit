0bfeb37916bed81085c60986b918b7a6a8bef967
Drew Skwiers-Koballa <drew.koballa@gmail.com>
2018-12-12 12:09:19 -0600

    working blitzindex vs database via context menu

--- src/extension.ts
+++ src/extension.ts
@@ -4,7 +4,7 @@ import * as request from 'request-promise-native';
 import * as sqlops from 'sqlops';
 import { error } from 'util';
 import {placeScript} from './placescript';
-
+import {BlitzIndex} from './blitzindex';
 
 export function activate(context: vscode.ExtensionContext) {
     const baseUrl = "https://raw.githubusercontent.com/BrentOzarULTD/SQL-Server-First-Responder-Kit/master/";
@@ -100,19 +100,44 @@ export function activate(context: vscode.ExtensionContext) {
     context.subscriptions.push(disposable_spblitzindex);
     
     //creating the quickrun script
-    var runspblitzindex = async () => {
+    var runspblitzindex = async (context?: sqlops.ObjectExplorerContext) => {
         console.log('Preparing sample run script.');
         let fileName = "exec_sp_blitzindex.sql";
-        const scriptText = `EXEC [dbo].[sp_BlitzIndex]
-        @DatabaseName = '',
-        --@TableName = '',
-         @Mode = 4
-         --0=Diagnose, 1=Summarize, 2=Index Usage Detail, 3=Missing Index Detail, 4=Diagnose Details`;
-         //await placeScript.placescript(fileName, scriptText);
-         new placeScript().placescript(fileName,scriptText);
+        let scriptText = '';
+        if (context) {
+            let scriptType = context.nodeInfo.nodeType;
+            switch (scriptType) {
+                case "Database": {
+                    scriptText = `EXEC [dbo].[sp_BlitzIndex]
+                        @DatabaseName = '${context.nodeInfo.label}',
+                        --@TableName = '',
+                        @Mode = 4
+                        --0=Diagnose, 1=Summarize, 2=Index Usage Detail, 3=Missing Index Detail, 4=Diagnose Details`;
+                    break;
+                }
+                default: {
+                    scriptText = `EXEC [dbo].[sp_BlitzIndex]
+                        @DatabaseName = '',
+                        --@TableName = '',
+                        @Mode = 4
+                        --0=Diagnose, 1=Summarize, 2=Index Usage Detail, 3=Missing Index Detail, 4=Diagnose Details`;
+                    break;
+                }
+            }
+        } else {
+            scriptText = `EXEC [dbo].[sp_BlitzIndex]
+                @DatabaseName = '',
+                --@TableName = '',
+                @Mode = 4
+                --0=Diagnose, 1=Summarize, 2=Index Usage Detail, 3=Missing Index Detail, 4=Diagnose Details`;
+        }
+        new placeScript().placescript(fileName,scriptText);
+        
     };
     var disposable_runspblitzindex = vscode.commands.registerCommand('extension.run_sp_blitzindex', runspblitzindex);
     context.subscriptions.push(disposable_runspblitzindex);
+
+    //sqlops.tasks.registerTask('extension.run_sp_blitzindex', (profile: sqlops.IConnectionProfile, context?: sqlops.ObjectExplorerContext) => new BlitzIndex().spBlitzIndexTable(profile, context));
 
     //creating the quickrun script for blitzcache
     var runspblitzcache = async () => {
@@ -145,25 +170,44 @@ export function activate(context: vscode.ExtensionContext) {
     context.subscriptions.push(disposable_runspblitzfirst);
 
 
+    let disposable = vscode.commands.registerCommand('extension.testmenus', async (context?: sqlops.ObjectExplorerContext) => {
+        await getDB(context);
+        // let connection = await getConnection(context);
 
-    // places scriptText into fileName editor with current connection
-    // async function placescript(fileName, scriptText) {
-    //     var setting: vscode.Uri = vscode.Uri.parse("untitled:" + fileName);
-    //     try {
-    //         let connection = await sqlops.connection.getCurrentConnection();
-    //         let doc = await vscode.workspace.openTextDocument(setting);
-    //         let editor = await vscode.window.showTextDocument(doc, 1, false);
-    //         editor.edit(edit => {
-    //             edit.insert(new vscode.Position(0, 0), scriptText);
-    //         });
-    //         if (connection) {
-    //             await sqlops.queryeditor.connect(doc.uri.toString(), connection.connectionId);
-    //         }
-    //     } catch (err) {
-    //         vscode.window.showErrorMessage(err);
-    //     }
-    // }
+        // // Format the message
+        // let message = `Hello Connected World! Please connect in Object Explorer or a Query window`;
+        // if (connection) {
+        //     message = `Hello Connected World! Your server name is ${connection.options['server']}`;
+        // }
+        // // Display a message box to the user
+        // vscode.window.showInformationMessage(message);
+    });
 
+    context.subscriptions.push(disposable);
+
+}
+
+async function getDB(context?: sqlops.ObjectExplorerContext) {
+    if (context) {
+        vscode.window.showInformationMessage(context.nodeInfo.nodeType);
+    }
+}
+
+async function getConnection(context?: sqlops.ObjectExplorerContext): Promise<sqlops.ConnectionInfo> {
+    // If we are called from a context menu use the predefined connection.
+    // This even has correct database if the node clicked on is under a specific DB
+    if (context) {
+        return context.connectionProfile;
+    }
+    // Otherwise use APIs to find the global current connection / active connection
+    let connection = await sqlops.connection.getCurrentConnection();
+    if (!connection) {
+        let allConnections = await sqlops.connection.getActiveConnections();
+        if (allConnections && allConnections.length > 0) {
+            connection = allConnections[0];
+        }
+    }
+    return connection;
 }
 
 // this method is called when your extension is deactivated
